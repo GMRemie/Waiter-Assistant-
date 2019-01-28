9c7892c3dda66dd877c79845faff0163d7942b21
Joseph <gmremcorat@gmail.com>
2019-01-27 21:51:04 -0500

    managers can now create items for specific menues

---
+++ Waiter Assistant/Waiter Assistant/ItemCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ItemCollectionViewCell.swift
+//  Waiter Assistant
+//
+//  Created by Joseph Storer on 1/27/19.
+//  Copyright © 2019 Joseph Storer. All rights reserved.
+//
+
+import UIKit
+
+class ItemCollectionViewCell: UICollectionViewCell {
+    
+}
